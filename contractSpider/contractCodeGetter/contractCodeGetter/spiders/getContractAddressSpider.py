dcc126e579559bb91a60ee4d8f0f71327d0cc642
xiaofeng <1506010130@hhu.edu.cn>
2020-09-08 16:12:48 +0800

    update step.txt-Describe the difficulties

--- contractSpider/contractCodeGetter/contractCodeGetter/spiders/getContractAddressSpider.py
+++ contractSpider/contractCodeGetter/contractCodeGetter/spiders/getContractAddressSpider.py
@@ -3,13 +3,10 @@ import scrapy
 
 class GetcontractaddressspiderSpider(scrapy.Spider):
     name = 'getContractAddressSpider'
-    allowed_domains = ['cn.etherscan.com/tokens']
+    #allowed_domains = ['https://cn.bing.com/']
     #start_urls = ['https://cn.etherscan.com/tokens?p=1/']
-    start_urls = ['https://cn.etherscan.com/tokens?p=1']
+    start_urls = ['https://cn.etherscan.com/contractsVerified']
 
     #解析爬取结果，response是返回对象
     def parse(self, response):
-    	fname = response[:-1]
-    	with open(fname + ".html",  "wb") as f:
-    		f.write(response.body)
-    	print("hahahahahaha")
+    	print(response.body)
