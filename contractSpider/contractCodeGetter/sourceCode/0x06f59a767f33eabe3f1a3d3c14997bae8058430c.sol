39eb8d0796938f1b6bcdcf2fd99ab8089eb6f1ec
xiaofeng <1506010130@hhu.edu.cn>
2020-10-12 10:11:38 +0800

    delete .sol files

--- contractSpider/contractCodeGetter/sourceCode/0x06f59a767f33eabe3f1a3d3c14997bae8058430c.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.7.0;
-
-import "./StandartToken.sol";
-import "./Details.sol";
-
-contract USDN is StandartToken, Detailable {}
--- contractSpider/contractCodeGetter/sourceCode/0x613f732650fe70dd1a97fee3e260a369aabbdca4.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.6.0;
-
-import "ERC20.sol";
-
-contract LFGToken is ERC20 {
-    constructor() ERC20("Alphabets", "LFG") public {
-        _mint(msg.sender, 100000000000000000000000000);
-    }
-}
