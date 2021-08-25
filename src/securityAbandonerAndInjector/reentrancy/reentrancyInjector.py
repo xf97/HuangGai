b9e4682cafa3dd7e67b3b6e1b203072bc4a9c499
Xiao Harley <harleyxiao@foxmail.com>
2021-08-25 21:43:35 +0800

    Update reentrancyInjector.py

--- src/securityAbandonerAndInjector/reentrancy/reentrancyInjector.py
+++ src/securityAbandonerAndInjector/reentrancy/reentrancyInjector.py
@@ -18,6 +18,15 @@ INJECTED_CONTRACT_SUFFIX = "_reentrancy.sol"
 INJECTED_INFO_SUFFIX = "_reentrancyInfo.txt"
 #结果保存路径
 DATASET_PATH = "./dataset/"
+#屏蔽信息保存位置
+SHIELD_INFO_PATH = "../../contractExtractor/reentrancyExtractor/result"
+#键值
+SRC_LIST_KEY = "srcList"
+#真值
+#bool真值字符串
+BOOL_TRUE_STR = "true"
+#替换为真值flag
+BOOL_TRUE_FLAG = 0
 
 #记录结构体
 class statementInfo:
@@ -28,10 +37,11 @@ class statementInfo:
 		transferState = str()	#转账语句
 
 class reentrancyInjector:
-	def __init__(self, _contractPath, _infoPath, _originalContractName):
+	def __init__(self, _contractPath, _infoPath, _shieldInfoPath, _originalContractName):
 		self.contractPath = _contractPath
 		self.infoPath = _infoPath
 		self.info = self.getInfoJson(self.infoPath)
+		self.shieldInfo = self.getInfoJson(_shieldInfoPath)
 		self.sourceCode = self.getSourceCode(self.contractPath)
 		self.preName = _originalContractName
 		try:
@@ -52,6 +62,16 @@ class reentrancyInjector:
 		except:
 			raise Exception("Failed to get source code when injecting.")
 			return str()
+
+	def initDict(self):
+		srcAndItsStr = dict()
+		#print(self.shieldInfo)
+		for item in self.shieldInfo[SRC_LIST_KEY]:
+			#print(item)
+			if item[2] == BOOL_TRUE_FLAG:
+				#布尔真值，执行替换
+				srcAndItsStr[item[0]] = [item[1], BOOL_TRUE_STR]
+		return srcAndItsStr
 
 	def inject(self):
 		infoDict = dict()
@@ -76,7 +96,11 @@ class reentrancyInjector:
 			item.transferState = transferState
 			infoDict[deductIndex] = item
 		#2. 根据不同路径，构造匹配语句并记录插入位置
-		injectInfo = dict()
+		#[bug fix] 添加屏蔽路径上require和assert语句
+		#print(infoDict, type(infoDict))
+		#injectInfo = dict()
+		injectInfo = self.initDict()
+		print(injectInfo)
 		#使用正则表达式，截取接收地址
 		addressPattern = re.compile(r"(\[)((\w)|(\.))+(\])")	#用于匹配接收转账的地址
 		for key in infoDict:
@@ -85,9 +109,18 @@ class reentrancyInjector:
 			#拼接插入语句
 			#额外的发币出去会有什么隐患？最影响的应该就是address.balance, address.balance并不能执行+-操作
 			sendEtherState = self.getSendEtherState(address)
-			injectInfo[key] = sendEtherState
+			injectInfo[key] = [key, sendEtherState]
+		#print(injectInfo)
 		#3. 根据源代码和插入位置，插入语句，并更新index
-		newSourceCode, newInjectInfo = self.injectStatements(injectInfo, self.sourceCode)
+		newSourceCode, newInjectInfo = self.insertStatement(injectInfo)
+		'''
+		#分不同种类的语句标注
+		for item in self.info[SRCLIST_KEY]:
+			if item[2] == BOOL_TRUE_FLAG:
+				#布尔真值，执行替换
+				srcAndItsStr[item[0]] = [item[1], BOOL_TRUE_STR]
+		newSourceCode, newInjectInfo = self.insertStatement(srcAndItsStr)
+		'''
 		#4. 输出并保存结果，然后产生自动标记
 		self.storeFinalResult(newSourceCode, self.preName)
 		#print(newSourceCode)
@@ -125,6 +158,22 @@ class reentrancyInjector:
 			f.writelines(labelLineList)
 		return
 
+	def insertStatement(self, _insertInfo):
+		tempCode = str()	
+		tempDict = copy.deepcopy(_insertInfo) #使用副本
+		startIndex = 0
+		indexList = sorted(_insertInfo.keys())
+		offset = list()
+		for index in indexList:
+			tempCode += self.sourceCode[startIndex: index] + _insertInfo[index][1]
+			startIndex = _insertInfo[index][0]
+			offset.append(len(_insertInfo[index][1]) + (_insertInfo[index][0] - index))
+			newIndex = index + sum(offset)
+			tempDict[newIndex] = tempDict.pop(index)
+		tempCode += self.sourceCode[startIndex:]
+		return tempCode, tempDict
+
+	'''
 	def injectStatements(self, _injectInfo, _sourceCode):
 		tempCode = str()	
 		tempDict = copy.deepcopy(_injectInfo) #使用副本
@@ -139,6 +188,7 @@ class reentrancyInjector:
 			tempDict[newIndex] = tempDict.pop(index)
 		tempCode += _sourceCode[startIndex:]
 		return tempCode, tempDict
+	'''
 
 	def getSendEtherState(self, _address):
 		if self.getSolcVersion(self.sourceCode) >= "0.7.0":
